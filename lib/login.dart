af4ec79499cbe52dde829331f234d3445b2392b6
Fateme Arjomandi <farjomandy76@gmail.com>
2022-10-10 15:10:32 +0330

    inital login page

---
+++ lib/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Login());
+}
+
+class Login extends StatefulWidget {
+  const Login({ Key? key }) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
