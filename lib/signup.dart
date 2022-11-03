ca93b73680d2e1cae84fe1aa3ff1805b9fe24a76
Fateme Arjomandi <farjomandy76@gmail.com>
2022-11-03 15:33:46 +0330

    initialTecrarKalmeVbor

--- lib/signup.dart
+++ lib/signup.dart
@@ -58,7 +58,7 @@ class _SignupState extends State<Signup> {
               height: 30,
             ),
             Container(
-              height: 350,
+              height: 420,
               margin: const EdgeInsets.fromLTRB(30, 0, 30, 0),
               child: Card(
                 shape: const RoundedRectangleBorder(
@@ -99,10 +99,82 @@ class _SignupState extends State<Signup> {
                         ),
                       ),
                     ),
-                    SizedBox(height: 30,),
-                     //کلمه عبور
-                     
+                    SizedBox(
+                      height: 30,
+                    ),
+                    //کلمه عبور
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(0, 0, 30, 0),
+                        child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              'کلمه عبور',
+                              style: inputTitle,
+                            ))),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                      child: TextField(
+                        keyboardType: TextInputType.none,
+                        maxLength: 11,
+                        textAlign: TextAlign.center,
+                        style: inputTitle,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: 'کلمه عبور',
+                            hintStyle: hintText,
+                            suffixIcon: Icon(Icons.lock)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    //تکرار کلمه عبور
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 30, 0),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          'تکرار کلمه عبور',
+                          style: inputTitle,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                      child: TextField(
+                        keyboardType: TextInputType.none,
+                        maxLength: 11,
+                        textAlign: TextAlign.center,
+                        style: inputTitle,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: 'تکرار کلمه عبور',
+                            hintStyle: hintText,
+                            suffixIcon: Icon(Icons.lock)),
+                      ),
+                    )
                   ]),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'ثبت نام',
+              ),
+              style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                backgroundColor: Colors.blueAccent,
+                fixedSize: const Size(330, 60),
+                primary: Colors.white,
+                textStyle: const TextStyle(
+                  fontFamily: 'Irs',
+                  fontSize: 22,
                 ),
               ),
             )
@@ -111,117 +183,4 @@ class _SignupState extends State<Signup> {
       ),
     );
   }
-/*Widget LoginUI() {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            'فروشگاه آنلاین من',
-            style: titleStyle,
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            height: 300,
-            margin: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-            child: Card(
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10))),
-              elevation: 4,
-              child: Center(
-                child: Column(
-                  children: const <Widget>[
-                    SizedBox(
-                      height: 30,
-                    ),
-
-                    //نام کاربری
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 30, 0),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          'نام کاربری',
-                          style: inputTitle,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
-                      child: TextField(
-                        maxLength: 11,
-                        keyboardType: TextInputType.phone,
-                        obscureText: true,
-                        textAlign: TextAlign.center,
-                        style: inputTitle,
-                        decoration: InputDecoration(
-                            hintText: 'شماره موبایل',
-                            hintStyle: hintText,
-                            suffixIcon: Icon(Icons.phone_iphone)),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    //کلمه عبور
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 30, 0),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          'کلمه عبور',
-                          style: inputTitle,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
-                      child: TextField(
-                        maxLength: 11,
-                        keyboardType: TextInputType.none,
-                        obscureText: true,
-                        textAlign: TextAlign.center,
-                        style: inputTitle,
-                        decoration: InputDecoration(
-                            hintText: 'کلمه عبور',
-                            hintStyle: hintText,
-                            suffixIcon: Icon(Icons.lock)),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          TextButton(
-              onPressed: () {},
-              child: const Text('ورود'),
-              style: TextButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  backgroundColor: Colors.blueAccent,
-                  fixedSize: const Size(330, 60),
-                  primary: Colors.white,
-                  textStyle: const TextStyle(
-                    fontFamily: 'Irs',
-                    fontSize: 22,
-                  ))),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            'حساب کاربری ندارم',
-            style: inputTitle,
-          )
-        ],
-      ),
-    );
-  }*/
 }
